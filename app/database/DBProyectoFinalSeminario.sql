623558cbdf0cf44a51b20256ef112d402b54954a
Pablo Barzola <robertho090504@gmail.com>
2024-10-29 21:37:00 -0500

    Cambios en la base de datos de los 3 ficheros

--- app/database/DBProyectoFinalSeminario.sql
+++ app/database/DBProyectoFinalSeminario.sql
@@ -6,7 +6,8 @@ USE sportsfieldrentaldb;
 DROP TABLE IF EXISTS acciones;
 CREATE TABLE acciones (
   idAccion 	INT AUTO_INCREMENT PRIMARY KEY,
-  nombre 	VARCHAR (20) NOT NULL
+  nombre 	VARCHAR (20) NOT NULL,
+  create_at 	DATETIME NOT NULL DEFAULT NOW()
 ) ENGINE = INNODB;
 
 -- T2 TIPO DE USUARIOS
@@ -27,6 +28,7 @@ CREATE TABLE permisos (
   idPermiso 	INT AUTO_INCREMENT PRIMARY KEY,
   idAccion 	INT NOT NULL,
   idTipoUsuario INT NOT NULL,
+  create_at 	DATETIME NOT NULL DEFAULT NOW(),
   CONSTRAINT 	fk_permisos_accion 		FOREIGN KEY (idAccion) REFERENCES acciones (idAccion),
   CONSTRAINT 	fk_permisos_tipo_usuario 	FOREIGN KEY (idTipoUsuario) REFERENCES tipos_usuarios (idTipoUsuario)
 ) ENGINE = INNODB;
@@ -72,7 +74,8 @@ CREATE TABLE campos (
   longitud 	DECIMAL (9, 6) NOT NULL,
   direccion 	VARCHAR (30) NOT NULL,
   distrito 	VARCHAR (30) NOT NULL,
-  telefono 	VARCHAR (30) NOT NULL
+  telefono 	VARCHAR (9) NULL,
+  create_at 		DATETIME 	NOT NULL DEFAULT NOW()
 ) ENGINE = INNODB;
 
 -- T7 ZONAS DE CAMPOS RELACIONADO CON T6
@@ -87,6 +90,7 @@ CREATE TABLE zonas_campos (
   precioHora 	SMALLINT NOT NULL,
   descripcion 	TEXT NULL,
   estado 	VARCHAR (10),
+  create_at 	DATETIME NOT NULL DEFAULT NOW(),
   CONSTRAINT 	fk_zonas_campos_campo FOREIGN KEY (idCampo) REFERENCES campos (idCampo)
 ) ENGINE = INNODB;
 
@@ -102,6 +106,7 @@ CREATE TABLE reservaciones (
   estadoPago 		VARCHAR (10) NOT NULL,
   precioHora 		SMALLINT NOT NULL,
   cantidadHora 		SMALLINT NOT NULL,
+  create_at 		DATETIME NOT NULL DEFAULT NOW(),
   CONSTRAINT 		fk_reservaciones_zona_campo 	FOREIGN KEY (idZonaCampo) REFERENCES zonas_campos (idZonaCampo),
   CONSTRAINT 		fk_reservaciones_usuario 	FOREIGN KEY (idUsuario) REFERENCES usuarios (idUsuario)
 ) ENGINE = INNODB;
@@ -109,12 +114,13 @@ CREATE TABLE reservaciones (
 -- T9 PAGOS RELACIONADO CON T8
 DROP TABLE IF EXISTS pagos;
 CREATE TABLE pagos (
-  idPago INT AUTO_INCREMENT PRIMARY KEY,
+  idPago 	INT AUTO_INCREMENT PRIMARY KEY,
   idReservacion INT NOT NULL,
-  monto DECIMAL (10, 2),
-  fechaPago DATETIME NOT NULL,
-  metodoPago VARCHAR (20) NOT NULL,
-  comprobante VARCHAR (255) NOT NULL,
-  estadoPago VARCHAR (20) NOT NULL,
+  monto 	DECIMAL (10, 2),
+  fechaPago 	DATETIME NOT NULL,
+  metodoPago 	VARCHAR (20) NOT NULL,
+  comprobante 	VARCHAR (255) NOT NULL,
+  estadoPago 	VARCHAR (20) NOT NULL,
+  create_at 	DATETIME NOT NULL DEFAULT NOW(),
   CONSTRAINT fk_pagos_reservacion FOREIGN KEY (idReservacion) REFERENCES reservaciones (idReservacion)
 ) ENGINE = INNODB;
