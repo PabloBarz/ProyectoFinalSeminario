9a343392416c0cdd25a6818e523dafa9fcce4b3d
Pablo Barzola <robertho090504@gmail.com>
2024-10-30 21:50:44 -0500

    Agrege una archivo views para las vistas reutilizables ne labd

---
+++ app/database/views_bd.sql
@@ -0,0 +1,22 @@
+USE sportsfieldrentaldb;
+
+/*Aqui iran todas las vista reutilizables*/
+
+DROP VIEW IF EXISTS vwUserPerson;  -- Eliminar la vista si ya existe
+CREATE VIEW vwUserPerson AS
+SELECT
+    US.idusuario,
+    PE.dni,
+    PE.apellidos, 
+    PE.nombres,
+    TU.nombreRol, 
+    TU.nombreCorto,
+    US.nomUser, 
+    US.passUser,
+    US.inactive_at 
+FROM 
+    usuarios US
+INNER JOIN 
+    personas PE ON PE.idpersona = US.idpersona
+INNER JOIN 
+    tipos_usuarios TU ON TU.idTipoUsuario = US.idTipoUsuario;
